f89633ff5ee45ed53ccbf5dd78277def091c52a2
Jasmin Trushna <Jasmintrushna@example.com>
2016-07-26 21:13:08 -0700

    calculator ex

---
+++ calculator.py
@@ -0,0 +1,34 @@
+def add (num1, num2):
+    total= num1 +num2
+    return total 
+
+def subtract (num1, num2):
+    dif= num1-num2
+    return dif 
+
+def multiply (num1, num2):
+    mult= num1* num2
+    return mult
+
+def divide(num1, num2):
+    float(num1)
+    div = (num1 / num2)
+    return div
+#why does it only return answer with one dec point?
+
+def modulo(num1, num2):
+    mod = num1 % num2
+    return mod
+
+def power(base, exponent):
+    pow= base** exponent
+    return pow
+
+def square(num):
+    sq= num**2
+    return sq
+
+# var1= add(4,5)
+# var2= subtract(9,6)
+# print add(var1,var2)
+# print add(add(4,5), subtract(9,6))
